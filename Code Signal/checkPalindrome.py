815f39bcd8fed334f90bdae0ea77ce02401e8608
dustinoy <55659114+dustinoy@users.noreply.github.com>
2019-10-12 17:47:55 +0800

    Create checkPalindrome.py

---
+++ Code Signal/checkPalindrome.py
@@ -0,0 +1,8 @@
+def checkPalindrome(inputString):
+    def reverse(inputString):
+        return inputString[::-1] 
+    if inputString == reverse(inputString):
+        return True
+    else:
+        return False
+        
