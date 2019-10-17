d093d81203be4f4211aaf8ffffad19d966e3b47d
dustinoy <55659114+dustinoy@users.noreply.github.com>
2019-10-17 13:35:19 +0800

    Update QuickSort.py

--- QuickSort/QuickSort.py
+++ QuickSort/QuickSort.py
@@ -1,18 +1,22 @@
-def QS(list):
-	left=[]
+list=[5,1,3,6,4,2,8,1,9]
+
+def QS(list): #定義Quicksort
+	left=[] #建立三個list
 	mid=[]
 	right=[]
-	if len(list)<=1:
+	if len(list)<=1: #假如list長度小於等於1就可以直接回傳
 		return list
 	else:
-		k=list[1]
+		k=list[1] #取list第2個數字當基準值
 		for i in list:
-			if i <k:
+			if i <k: #假如變數i小於基準值則放到左邊的list
 				left.append(i)
-			elif i >k:
+			elif i >k: #大於基準值則放到右邊的list
 				right.append(i)
-			else:
+			else: #其餘等於基準值的則都集合在同一個list
 				mid.append(i)
-	left = QS(left)
+	left = QS(left) #再把左右的list Quicksort直到不能再跑
 	right = QS(right)
-	return left+mid+right
+	return left+mid+right #最後總結三個部分list就整理好了
+
+QS(list)
